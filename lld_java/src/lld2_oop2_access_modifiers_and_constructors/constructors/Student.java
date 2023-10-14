5d4dc63d7afeda4411b168fe7ab8162b801359b7
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-15 09:11:22 +1030

    Understanding custom constructors, default constructors and their initialisation

---
+++ lld_java/src/lld2_oop2_access_modifiers_and_constructors/constructors/Student.java
@@ -0,0 +1,14 @@
+package lld2_oop2_access_modifiers_and_constructors.constructors;
+
+public class Student {
+    int age;
+    String name;
+    String gender;
+
+    Student(String  name, int age) {
+        System.out.println("Creating a new student" + this.age);
+        this.name = name;
+        this.age = age;
+    }
+
+}
