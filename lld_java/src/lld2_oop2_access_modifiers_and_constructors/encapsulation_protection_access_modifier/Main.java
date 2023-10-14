7fa94762dba83a2475a0befd69112fd82794296e
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-15 07:25:40 +1030

    Encapsulation via class where we protected attributes and actions via access modifiers

---
+++ lld_java/src/lld2_oop2_access_modifiers_and_constructors/encapsulation_protection_access_modifier/Main.java
@@ -0,0 +1,10 @@
+package lld2_oop2_access_modifiers_and_constructors.encapsulation_protection_access_modifier;
+
+public class Main {
+    public static void main (String[] args) {
+        Student st1 = new Student();
+
+        // st1.name = "Ankur"; // Not possible because name has private access
+        st1.age = 40;
+    }
+}
