614db0e8021518a249d7e6f346332ff94c0c35a6
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-23 11:44:39 +1030

    Learned method overriding

---
+++ lld_java/src/O5_14032023_lld_oop_method_overloading_overriding_interfaces/method_overriding/Animal.java
@@ -0,0 +1,7 @@
+package O5_14032023_lld_oop_method_overloading_overriding_interfaces.method_overriding;
+
+public class Animal {
+    void makeSound(int num) {
+        System.out.println("Bla bla bla " + num);
+    }
+}
