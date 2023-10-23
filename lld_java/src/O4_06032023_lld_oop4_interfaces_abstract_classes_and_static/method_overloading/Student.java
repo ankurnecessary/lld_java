aca95fa360995887a9ad3a6b81b05bca616a4996
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-23 11:43:28 +1030

    I just changed the method name as it was producing error in the project

--- lld_java/src/O4_06032023_lld_oop4_interfaces_abstract_classes_and_static/method_overloading/Student.java
+++ lld_java/src/O4_06032023_lld_oop4_interfaces_abstract_classes_and_static/method_overloading/Student.java
@@ -3,7 +3,7 @@ package O4_06032023_lld_oop4_interfaces_abstract_classes_and_static.method_overl
 import java.util.List;
 
 public class Student {
-    void test (List<String> strArr) {
+    void test1 (List<String> strArr) {
         System.out.println(strArr);
     }
 
