7a0f8f77bb75c12b606429c2b720cc3dce5e2ca6
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-24 10:16:46 +1030

    Created a Line class with Point class using composition

---
+++ lld_java/src/O5_practice/exercise3_composition_inheritance/Point.java
@@ -0,0 +1,37 @@
+package O5_practice.exercise3_composition_inheritance;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "Point: (" + this.x + "," + this.y + ")";
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
---
+++ lld_java/src/O5_practice/exercise3_composition_inheritance/TestPoint.java
@@ -0,0 +1,11 @@
+package O5_practice.exercise3_composition_inheritance;
+
+public class TestPoint {
+    public static void main(String[] args) {
+        Point p1 = new Point(10,20);
+        System.out.println(p1);
+
+        Point p2 = new Point(100, 10);
+        System.out.println(p2);
+    }
+}
