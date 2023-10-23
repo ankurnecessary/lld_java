983e3b3e4093d32c3605d2fef2a86acafbd769b3
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-23 22:53:51 +1030

    Practicing some external exercise for inheritance

---
+++ lld_java/src/O5_practice/exercise2_inheritance/Cylinder.java
@@ -0,0 +1,38 @@
+package O5_practice.exercise2_inheritance;
+
+public class Cylinder extends Circle{
+    private double height;
+
+    public Cylinder() {
+        super();
+        this.height = 1.0;
+    }
+
+    public Cylinder(double radius) {
+        super(radius);
+        this.height = 1.0;
+    }
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return super.getArea() * this.height;
+    }
+
+    @Override
+    public double getArea() {
+        return (2 * Math.PI * super.getRadius() * this.height) + (2 * super.getArea());
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder: Subclass of " + super.toString() + " height=" + height;
+    }
+}
