f2a662e2032d82969d3947670482889879b3ecf0
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-25 11:37:16 +1030

    Exercise on polymorphism, downcasting and abstract classes

---
+++ lld_java/src/O5_practice/exercise4_polymorphism/Animal.java
@@ -0,0 +1,5 @@
+package O5_practice.exercise4_polymorphism;
+
+abstract public class Animal {
+    abstract public void greeting();
+}
---
+++ lld_java/src/O5_practice/exercise4_polymorphism/Cat.java
@@ -0,0 +1,8 @@
+package O5_practice.exercise4_polymorphism;
+
+public class Cat extends Animal{
+    @Override
+    public void greeting() {
+        System.out.println("Meow!");
+    }
+}
