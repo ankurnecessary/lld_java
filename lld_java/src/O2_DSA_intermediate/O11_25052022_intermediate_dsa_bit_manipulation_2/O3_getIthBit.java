7a0c7b968f3c59450c8ae2c0a72d95c035167aba
Ankur Gupta <ankurnecessary@gmail.com>
2023-12-18 04:16:20 +1030

    Bit manipulation: Get ith bit of a number. Way 1 & 2

--- lld_java/src/O2_DSA_intermediate/O11_25052022_intermediate_dsa_bit_manipulation_2/O3_getIthBit.java
+++ lld_java/src/O2_DSA_intermediate/O11_25052022_intermediate_dsa_bit_manipulation_2/O3_getIthBit.java
@@ -6,25 +6,23 @@ package O2_DSA_intermediate.O11_25052022_intermediate_dsa_bit_manipulation_2;
 public class O3_getIthBit {
     public static void main(String[] args) {
         int num = 13; // 1101
-        int i = 3; // 4 -> 1, 5 -> 0
-        System.out.println(getIthBit(num, i));
+        int i = 1; // 4 -> 1, 5 -> 0
+        System.out.println(getIthBit1(num, i));
+        System.out.println(getIthBit2(num, i));
     }
 
     // Approach 2
-    private static int getIthBit(int num, int i) {
-        return (num >> (i - 1)) & 1;
+    private static int getIthBit1(int num, int i) {
+        return (num & (1 << (i - 1))) != 0 ? 1 : 0;
     }
+    // TC - O(1)
+    // SC - O(1)
 
     // Approach 1
-    /* private static int getIthBit(int num, int i) {
-        int ans = 0;
-        for(int j = 0; j < i; j++) {
-            ans = num & 1;
-            // System.out.print(ans);
-            num >>= 1;
-        }
-        // System.out.println();
-        return ans;
-    } */
+    private static int getIthBit2(int num, int i) {
+         return (num >> (i - 1)) & 1;
+    }
+    // TC - O(1)
+    // SC - O(1)
 
 }
