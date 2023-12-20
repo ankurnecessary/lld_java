f823b4c92a0cc47159bba77eef5152801ddc3709
Ankur Gupta <ankurnecessary@gmail.com>
2023-12-21 09:39:15 +1030

    String Mathematics: Some important string and char related operations

---
+++ lld_java/src/playground/StringMaths.java
@@ -0,0 +1,10 @@
+package playground;
+
+public class StringMaths {
+    public static void main(String[] args) {
+        System.out.println('A' - 'A');
+        System.out.println("A".charAt(0));
+        int num = '6' - '0';
+        System.out.println(num);
+    }
+}
