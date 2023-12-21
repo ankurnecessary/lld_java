512e01e32841fa51014d6593f70f47fa3fc17b56
Ankur Gupta <ankurnecessary@gmail.com>
2023-12-22 05:28:05 +1030

    String maths: Why in some question integer 48 is used instead of char '0'? Both the values are same

--- lld_java/src/playground/StringMaths.java
+++ lld_java/src/playground/StringMaths.java
@@ -2,9 +2,17 @@ package playground;
 
 public class StringMaths {
     public static void main(String[] args) {
+
         System.out.println('A' - 'A');
         System.out.println("A".charAt(0));
         int num = '6' - '0';
         System.out.println(num);
+
+        // Why in some question integer 48 is used instead of char '0'? Both the values are same
+        int n = '0';
+        System.out.println(n);
+        System.out.println("Some: " + ('9' - '0'));
+        System.out.println("Some: " + ('9' - 48));
+
     }
 }
