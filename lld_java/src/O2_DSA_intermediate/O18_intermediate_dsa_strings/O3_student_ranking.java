c9d19c3160ccf5ad03fff6b8581416de278ab6a0
Ankur Gupta <ankurnecessary@gmail.com>
2024-01-31 10:37:09 +1030

    DSA String: We are provided with an array of students who scored between 0 to 300. We need to sort the students on the basis of their score.

---
+++ lld_java/src/O2_DSA_intermediate/O18_intermediate_dsa_strings/O3_student_ranking.java
@@ -0,0 +1,11 @@
+package O2_DSA_intermediate.O18_intermediate_dsa_strings;
+
+public class O3_student_ranking {
+    public static void main(String[] args) {
+        System.out.println("We are provided with an array of students who scored between 0 to 300. " +
+                "We need to sort the students on the basis of their score");
+
+        System.out.println("We will use count sort to solve this problem " +
+                "because its TC is O(n + range(0 - 300)) and SC is O(range(0-300))");
+    }
+}
